SchaichAlonso/StaticsMapping

commit a6ea4a837bdc050d00df5c29c0404d7ff5a35835

Expose object fee/weight
Expose the object fee and adapt the test case to assert foreign objects are
heavier than owned ones.

Common/Classification/Afiliates.cpp
@@ -270,48 +270,42 @@ Classification::Afiliations::outdated (ObjectPointer obj, Airline::PrimaryKey us
 
 void
 Classification::Afiliations::mergeObject (
-    ObjectsByXPClass &objects,
-    CheapestByXPClass &prices,
-    ObjectPointer obj,
-    XPClass data,
-    Fee fee
+    WeightedObjectsByXPClass &objects,
+    XPClass xpclass,
+    WeightedObject object
 ) const
 {
-  if (prices.contains (data)) {
-    Fee current_cheapest = prices.value (data);
-    if (current_cheapest < fee) {
-      return;
-    }
-    if (fee < current_cheapest) {
-      objects.remove (data);
-      prices.insert (data, fee);
+  if (objects.contains(xpclass)) {
+    Fee current_cheapest = objects.value(xpclass).weight;
+    Fee candidate_price = object.weight;
+    
+    if (candidate_price <= current_cheapest) {
+      if (candidate_price < current_cheapest) {
+        objects.remove(xpclass);
+      }
+      objects.insert (xpclass, object);
     }
   } else {
-    prices.insert (data, fee);
+    objects.insert (xpclass, object);
   }
-  
-  objects.insert (data, obj);
 }
 
 
 
 void
 Classification::Afiliations::mergeObjects (
-    ObjectsByXPClass &objects,
-    CheapestByXPClass &prices,
-    ObjectPointer obj,
-    Fee fee
-) const
+    WeightedObjectsByXPClass &objects,
+    ObjectPointer object,
+    Fee initial) const
 {
+  QList<XPClass> xp_classes = 
+      XPClass::create (m_definitions->aircraft(object->aircraft()), object);
   
-  QList<XPClass> object_by_xp_classes = 
-      XPClass::create (m_definitions->aircraft(obj->aircraft()), obj);
-  
-  Q_FOREACH (XPClass data, object_by_xp_classes) {
+  Q_FOREACH (XPClass xp_class, xp_classes) {
     for (int resized=0; ; ++resized) {
-      mergeObject (objects, prices, obj, data, resizedFee (fee, resized));
-      if (data.couldBeLarger())
-        data=data.nextLarger();
+      mergeObject (objects, xp_class, WeightedObject(object, resizedFee(initial, resized)));
+      if (xp_class.couldBeLarger())
+        xp_class=xp_class.nextLarger();
       else
         break;
     }
@@ -320,39 +314,36 @@ Classification::Afiliations::mergeObjects (
 
 
 
-Classification::ObjectsByXPClass
-Classification::Afiliations::objectsAvailableToAirline (QString airline) const
+Classification::WeightedObjectsByXPClass
+Classification::Afiliations::objectsAvailable (QString airline) const
 {
-  CheapestByXPClass prices;
-  ObjectsByXPClass result;
-  Fees fees;
-  
-  fees = this->fees (airline);
-  
-  Q_FOREACH (ObjectPointer obj, m_definitions->objects()) {
-    if (outdated(obj, airline)) {
-      continue;
-    }
-    
-    QString owner = obj->livery ();
-    if (fees.contains (owner)) {
-      mergeObjects (result, prices, obj, fees.value (owner));
-    }
-  }
-  
-  return (result);
+  return (objectsAvailable(m_definitions->airline(airline)));
 }
 
 
 
-Classification::ObjectsByXPClass
-Classification::Afiliations::objectsAvailable () const
+Classification::WeightedObjectsByXPClass
+Classification::Afiliations::objectsAvailable (AirlinePointer airline) const
 {
-  CheapestByXPClass prices;
-  ObjectsByXPClass result;
+  WeightedObjectsByXPClass result;
   
-  Q_FOREACH (ObjectPointer obj, m_definitions->objects()) {
-    mergeObjects (result, prices, obj, 0);
+  if (airline) {
+    Fees fees = this->fees(airline->primaryKey());
+  
+    Q_FOREACH (ObjectPointer obj, m_definitions->objects()) {
+      if (outdated(obj, airline->primaryKey())) {
+        continue;
+      }
+    
+      QString owner = obj->livery();
+      if (fees.contains(owner)) {
+        mergeObjects (result, obj, fees.value(owner));
+      }
+    }
+  } else {
+    Q_FOREACH (ObjectPointer obj, m_definitions->objects()) {
+      mergeObjects (result, obj, 0);
+    }
   }
   
   return (result);
@@ -367,12 +358,13 @@ Classification::Afiliations::library () const
   
   Q_FOREACH (AirlinePointer airline, m_definitions->airlines()) {
     QString icao = airline->icao ();
-    if (not airline->isFictiveIcaoCode(icao)) {
-      retval.add (objectsAvailableToAirline(icao), icao);
+    
+    if (not airline->isFictiveIcaoCode()) {
+      retval.add (objectsAvailable(airline));
     }
   }
 
-  retval.add (objectsAvailable());
+  retval.add (objectsAvailable(AirlinePointer()));
   
   return (retval);
 }

Test/Afiliates.cpp
@@ -7,6 +7,19 @@
 #include "Afiliates.hpp"
 
 
+Classification::WeightedObject::Weight
+Classification::Test::Afiliates::weight (QList<WeightedObject> list, ObjectPointer object)
+{
+  Q_FOREACH (const Classification::WeightedObject &candidate, list) {
+    if (candidate.object == object) {
+      return (candidate.weight);
+    }
+  }
+  
+  return (-1);
+}
+
+
 void
 Classification::Test::Afiliates::init ()
 {
@@ -52,15 +65,15 @@ Classification::Test::Afiliates::basic ()
   addObject ("FFM", "AT75", "x", 1, false);
   
   Classification::Afiliations a (m_definitions);
-  ObjectsByXPClass afl = a.objectsAvailableToAirline("AFL");
-  ObjectsByXPClass ffm = a.objectsAvailableToAirline("FFM");
-  ObjectsByXPClass mas = a.objectsAvailableToAirline("MAS");
+  WeightedObjectsByXPClass afl = a.objectsAvailable("AFL");
+  WeightedObjectsByXPClass ffm = a.objectsAvailable("FFM");
+  WeightedObjectsByXPClass mas = a.objectsAvailable("MAS");
   
   XPClass at75 (Object::Airliner, Aircraft::Turboprop, Aircraft::SizeC);
   
   QVERIFY (afl.isEmpty());
   QVERIFY (ffm.contains(at75));
-  QVERIFY (ffm == mas);
+  QVERIFY (mas.contains(at75));
 }
 
 
@@ -81,8 +94,8 @@ Classification::Test::Afiliates::sizing ()
   
   
   Classification::Afiliations a (m_definitions);
-  ObjectsByXPClass ffm = a.objectsAvailableToAirline("FFM");
-  ObjectsByXPClass mas = a.objectsAvailableToAirline("MAS");
+  WeightedObjectsByXPClass ffm = a.objectsAvailable("FFM");
+  WeightedObjectsByXPClass mas = a.objectsAvailable("MAS");
   
   
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeA)).size() == 0);
@@ -92,10 +105,10 @@ Classification::Test::Afiliates::sizing ()
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeE)).size() == 1);
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeF)).size() == 1);
   
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeC)).contains(at75));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeD)).contains(at75));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeE)).contains(at75));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeF)).contains(at75));
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeC)), at75) >= 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeD)), at75) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeE)), at75) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turboprop, Aircraft::SizeF)), at75) > 0);
   
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeA)).size() == 0);
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeB)).size() == 1);
@@ -104,20 +117,20 @@ Classification::Test::Afiliates::sizing ()
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeE)).size() == 2);
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 2);
   
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeB)).contains(e135));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeC)).contains(e75l));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeC)).contains(su95));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeD)).contains(e75l));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeD)).contains(su95));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeE)).contains(e75l));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeE)).contains(su95));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(e75l));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(su95));
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeB)), e135) >= 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeC)), e75l) >= 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeC)), su95) >= 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeD)), e75l) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeD)), su95) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeE)), e75l) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeE)), su95) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), e75l) > 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), su95) > 0);
   
   QVERIFY (mas.size() == ffm.size());
-  for (ObjectsByXPClass::ConstIterator i = ffm.constBegin(); i!=ffm.constEnd(); ++i) {
+  for (WeightedObjectsByXPClass::ConstIterator i = ffm.constBegin(); i!=ffm.constEnd(); ++i) {
     QVERIFY (mas.contains (i.key()));
-    QVERIFY (mas.values(i.key()).contains(i.value()));
+    QVERIFY (weight(mas.values(i.key()), i.value().object) > i.value().weight);
   }
 }
 
@@ -143,9 +156,9 @@ Classification::Test::Afiliates::explicitAge ()
   
   a.setYear (1995);
   
-  ObjectsByXPClass ffm = a.objectsAvailableToAirline("FFM");
+  WeightedObjectsByXPClass ffm = a.objectsAvailable("FFM");
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 1);
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(object(2)));
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), object(2)) >= 0);
 }
 
 
@@ -162,22 +175,22 @@ Classification::Test::Afiliates::implicitAge ()
   
   a.setYear (2005);
   
-  ObjectsByXPClass ffm, mas;
+  WeightedObjectsByXPClass ffm, mas;
   
-  ffm = a.objectsAvailableToAirline("FFM");
+  ffm = a.objectsAvailable("FFM");
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 1);
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(object(1)));
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), object(1)) >= 0);
   
   m_definitions->airline("FFM")->setFounded (2007);
   m_definitions->airline("MAS")->setFounded (1947);
   
-  ffm = a.objectsAvailableToAirline("FFM");
+  ffm = a.objectsAvailable("FFM");
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 0);
   
   /*
    * it's FFM's object, so MAS doesn't get it even though MAS is old enough.
    */
-  mas = a.objectsAvailableToAirline("MAS");
+  mas = a.objectsAvailable("FFM");
   QVERIFY (mas.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 0);
   
   
@@ -186,12 +199,12 @@ Classification::Test::Afiliates::implicitAge ()
   m_definitions->airline("MAS")->setCeased (2010);
   
   a.setYear (2011);
-  ffm = a.objectsAvailableToAirline("FFM");
-  mas = a.objectsAvailableToAirline("MAS");
+  ffm = a.objectsAvailable("FFM");
+  mas = a.objectsAvailable("MAS");
   
   QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 2);
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(object(1)));
-  QVERIFY (ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).contains(object(2)));
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), object(1)) >= 0);
+  QVERIFY (weight(ffm.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)), object(2)) >= 0);
   QVERIFY (mas.values(XPClass(Object::Airliner, Aircraft::Turbofan, Aircraft::SizeF)).size() == 0);
 }
 

Common/Classification/Afiliates.hpp
@@ -8,6 +8,7 @@
 #include "Airline.hpp"
 #include "Airport.hpp"
 #include "Definitions.hpp"
+#include "WeightedObject.hpp"
 #include "XPClass.hpp"
 #include "XPLibrary.hpp"
 
@@ -15,7 +16,7 @@ namespace Classification
 {
   struct Afiliations
   {
-    typedef int Fee;
+    typedef WeightedObject::Weight Fee;
     typedef QPair<Airline::PrimaryKey, Airline::PrimaryKey> AirlinePair;
     typedef QPair<Airport::PrimaryKey, Airport::PrimaryKey> AirportPair;
     typedef QMap<AirportPair, Airport::DistanceInKM> AirportDistanceCache;
@@ -32,8 +33,9 @@ namespace Classification
     HubDistances hubDistances () const;
     Tree tree () const;
     Fees fees (QString) const;
-    ObjectsByXPClass objectsAvailableToAirline (QString) const;
-    ObjectsByXPClass objectsAvailable () const;
+    
+    WeightedObjectsByXPClass objectsAvailable (QString) const;
+    WeightedObjectsByXPClass objectsAvailable (AirlinePointer) const;
     
     XPLibrary library () const;
     
@@ -49,8 +51,8 @@ namespace Classification
     
     Airport::DistanceInKM shortestDistance (AirportDistanceCache &, QStringList, QStringList) const;
     
-    void mergeObject  (ObjectsByXPClass &, CheapestByXPClass &, ObjectPointer, XPClass, Fee) const;
-    void mergeObjects (ObjectsByXPClass &, CheapestByXPClass &, ObjectPointer, Fee) const;
+    void mergeObject (WeightedObjectsByXPClass &, XPClass, WeightedObject) const;
+    void mergeObjects (WeightedObjectsByXPClass &, ObjectPointer, Fee) const;
     
   protected:
     DefinitionsPointer m_definitions;

Common/Classification/XPLibrary.cpp
@@ -28,10 +28,10 @@ Classification::XPLibrary::add (XPClass xp_class, ObjectPointer obj, QString air
 
 
 void
-Classification::XPLibrary::add (ObjectsByXPClass objs, QString airline)
+Classification::XPLibrary::add (WeightedObjectsByXPClass objs, QString airline)
 {
-  for (ObjectsByXPClass::ConstIterator i=objs.constBegin(); i!=objs.constEnd(); ++i) {
-    add (i.key(), i.value(), airline);
+  for (WeightedObjectsByXPClass::ConstIterator i=objs.constBegin(), e=objs.constEnd(); i!=e; ++i) {
+    add (i.key(), i.value().object, airline);
   }
 }
 

Common/Classification/XPLibrary.hpp
@@ -3,6 +3,7 @@
 #include <QtCore/QString>
 
 #include "Classification.hpp"
+#include "WeightedObject.hpp"
 #include "XPClass.hpp"
 
 
@@ -14,7 +15,7 @@ namespace Classification
    ~XPLibrary ();
     
     void add (XPClass, ObjectPointer, QString =QString());
-    void add (ObjectsByXPClass, QString =QString());
+    void add (WeightedObjectsByXPClass, QString =QString());
     
     QString toString (bool) const;
     

Test/Afiliates.hpp
@@ -1,5 +1,7 @@
 #include <QtTest>
 
+#include <Classification/WeightedObject.hpp>
+
 #include "DefinitionsHelper.hpp"
 
 namespace Classification
@@ -9,6 +11,9 @@ namespace Classification
     struct Afiliates : DefinitionsHelper
     {
       Q_OBJECT
+    protected:
+      static WeightedObject::Weight weight(QList<WeightedObject> list, ObjectPointer object);
+    
     private slots:
       virtual void init () Q_DECL_OVERRIDE;
       

commit 4a2daea717aafd3f781cf6b684c531c137f0119b

Have airline ICAO be appended to primary exports
Regression had been introduced in a6ea4a8.

Common/Classification/Afiliates.cpp
@@ -357,10 +357,8 @@ Classification::Afiliations::library () const
   XPLibrary retval (m_definitions);
   
   Q_FOREACH (AirlinePointer airline, m_definitions->airlines()) {
-    QString icao = airline->icao ();
-    
     if (not airline->isFictiveIcaoCode()) {
-      retval.add (objectsAvailable(airline));
+      retval.add (objectsAvailable(airline), airline->icao());
     }
   }
 