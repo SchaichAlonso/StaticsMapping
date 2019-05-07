36ace1098e422b342da2c72166097856e4c0db06
Schaich <alonso@freebsd.org>
2019-05-07 08:34:51 +0200

    Enable run-time overriding the data path

--- Common/DataPath.cpp
+++ Common/DataPath.cpp
@@ -3,12 +3,16 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDir>
 #include <QtCore/QFile>
+#include <QtCore/QtGlobal>
 
 #include "DataPath.hpp"
 
 QString
 DataPath::path(QString filename)
 {
+  if (qEnvironmentVariableIsSet("SHARED_DATA_PATH")) {
+    return QDir(qEnvironmentVariable("SHARED_DATA_PATH")).filePath(filename);
+  }
   QDir path(QCoreApplication::applicationDirPath());
   path.cdUp();
   path.cd(SHARED_DATA_PATH);
