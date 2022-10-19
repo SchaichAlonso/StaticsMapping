0b85b72475156ad205e1461055df1dedea164f66
Alonso Schaich <alonsoschaich@fastmail.fm>
2022-10-19 16:27:20 +0000

    Print full file path in the diagnostic

--- Common/DataPath.cpp
+++ Common/DataPath.cpp
@@ -12,7 +12,7 @@ DataPath::path(QString filename)
   QDir path(QCoreApplication::applicationDirPath());
   path.cdUp();
   path.cd(SHARED_DATA_PATH);
-  return (path.filePath(filename));
+  return (path.absoluteFilePath(filename));
 }
 
 QString
@@ -21,8 +21,9 @@ DataPath::existingPath(QString filename)
   QString candidate(path(filename));
   if(!QFile::exists(candidate)) {
     throw std::runtime_error(
-      QString("File <%1> does not exist")
+      QString("File <%2> resolved from <%1> does not exist")
         .arg(filename)
+        .arg(candidate)
         .toStdString()
     );
   }
